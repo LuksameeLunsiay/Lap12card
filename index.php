ed776c307ebc1bd3478a35532e05a0a6b2e133a4
LuksameeLunsiay <73680084+LuksameeLunsiay@users.noreply.github.com>
2020-11-06 19:58:36 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,32 +8,35 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 </head>
 <body>
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-4">
-                <div class="card">
-                        <img class="card-img-top" src="122.jpg">
-                    <div class="card-body">
-                        <h2>asdfg</h2>
-                        <p>Beautiful Sunset need Cloudy Sky</p>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-4">
+                    <div class="card">
+                            <img class="card-img-top" src="1211.jpg">
+                        <div class="card-body">
+                            <h2>Man in a room</h2>
+                            <p>Beautiful Sunset need Cloudy Sky</p>
+                        <a href="#" class="btn btn-primary">Read more</a>
                     </div>
                 </div>
-            </div>
-            <div class="col-sm-4">
-                <div class="card">
-                        <img class="card-img-top" src="123.jpg">
-                    <div class="card-body">
-                        <h2>asdfg</h2>
-                        <p>It is not an attitude; it is the way I am</p>
+                    </div>
+                    <div class="col-sm-4">
+                        <div class="card">
+                                <img class="card-img-top" src="1233.jpg">
+                            <div class="card-body">
+                                <h2>Young master</h2>
+                                <p>It is not an attitude; it is the way I am</p>
+                            <a href="#" class="btn btn-primary">Read more</a>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-sm-4">
-                <div class="card">
-                        <img class="card-img-top" src="121.jpg">
-                    <div class="card-body">
-                        <h2>asdfg</h2>
-                        <p>Sunshine mixed with a little hurricane</p>
+                <div class="col-sm-4">
+                    <div class="card">
+                            <img class="card-img-top" src="1222.jpg">
+                        <div class="card-body">
+                            <h2>Sky</h2>
+                            <p>Sunshine mixed with a little hurricane</p>
+                        <a href="#" class="btn btn-primary">Read more</a>
                     </div>
                 </div>
             </div>
